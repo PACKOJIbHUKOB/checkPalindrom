7db25e4ad4060162076ec07eb33c0527de82d263
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-07-20 16:43:20 +0300

    Задание билета №2 проверка строки на полиндром!

--- src/main.java
+++ src/main.java
@@ -1,24 +1,20 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println(checkPalindrom("Ароза упала на лапу азора"));
+        System.out.println(checkPalindrom("А роза упала на лапу азора"));
         System.out.println(checkPalindrom("кабан упал и лапу набок"));
         System.out.println(checkPalindrom("шабаш"));
         System.out.println(checkPalindrom("ABcbA"));
     }
     private static boolean checkPalindrom (String word){
-        char [] chars = word.toLowerCase().toCharArray();
+        char [] chars = word.replace(" ","").toLowerCase().toCharArray();
         int left = 0;
         int right = chars.length -1;
         while (left<right){
             if (chars[left]!=chars[right]) {
                 return false;
             }
-            do{
-                left++;
-            }while (left<right&&chars[left]==' ');
-            do {
-                --right;
-            }while (right>left&&chars[right]==' ');
+            left++;
+            --right;
         }
         return true;
     }
